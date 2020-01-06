57626ef955099770d029b738a2dfd263022b61dd
Peter Weiss <peter.weiss.1969@gmail.com>
2020-01-06 11:41:58 +0100

    KNX: Send TUNNEL ACK also if received and last sequence number are identical

--- src/knx.cpp
+++ src/knx.cpp
@@ -445,16 +445,18 @@ Events KnxHandler::receiveX(const Items& items)
 
 		Byte seqNo = msg[8];
 		Byte expectedSeqNo = (lastReceivedSeqNo + 1) & 0xFF;
-		if (seqNo == lastReceivedSeqNo)
-		{
-			logger.warn() << "Received TUNNEL REQUEST has last sequence number " << cnvToHexStr(seqNo)
-			              << " (expected: " << cnvToHexStr(expectedSeqNo) << ")" << endOfMsg();
-			return events;
-		}
 		if (seqNo != expectedSeqNo)
 		{
 			logger.warn() << "Received TUNNEL REQUEST has invalid sequence number " << cnvToHexStr(seqNo)
 			              << " (expected: " << cnvToHexStr(expectedSeqNo) << ")" << endOfMsg();
+			return events;
+		}
+		if (seqNo == lastReceivedSeqNo)
+		{
+			logger.warn() << "Received TUNNEL REQUEST has last sequence number " << cnvToHexStr(seqNo)
+			              << " (expected: " << cnvToHexStr(expectedSeqNo) << ")" << endOfMsg();
+
+			sendDataMsg(createTunnelAck(seqNo));
 			return events;
 		}
 		lastReceivedSeqNo = seqNo;
