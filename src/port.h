43d393f4f866db8fc5422f24520cbf34643a38c0
Peter Weiss <peter.weiss.1969@gmail.com>
2020-08-18 22:15:57 +0200

    Fix for port error counter

--- src/port.h
+++ src/port.h
@@ -82,7 +82,7 @@ public:
 	PortHandler(string _id, PortConfig _config, Logger _logger);
 	virtual ~PortHandler();
 	virtual bool supports(EventType eventType) const override { return eventType == EventType::STATE_IND; }
-	virtual HandlerState getState() const override { return HandlerState(); }
+	virtual HandlerState getState() const override { return handlerState; }
 	virtual long collectFds(fd_set* readFds, fd_set* writeFds, fd_set* excpFds, int* maxFd) override;
 	virtual Events receive(const Items& items) override;
 	virtual Events send(const Items& items, const Events& events) override { return Events(); }
