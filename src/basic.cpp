6fb96affab9d180a9bcf829c2a0b43c6e4bef2c0
peter <peter@Peters-PC.fritz.box>
2022-03-27 13:37:30 +0200

    Fix
    Take mkTime() and localtime() from the same namespace

--- src/basic.cpp
+++ src/basic.cpp
@@ -67,7 +67,7 @@ bool TimePoint::fromStr(string timePointStr, TimePoint& timePoint, string timePo
 	stream >> std::get_time(&tp, timePointFormat.c_str());
 	if (stream.fail())
 		return false;
-	timePoint = TimePoint(std::chrono::system_clock::from_time_t(mktime(&tp)));
+	timePoint = TimePoint(std::chrono::system_clock::from_time_t(std::mktime(&tp)));
 	return true;
 }
 
